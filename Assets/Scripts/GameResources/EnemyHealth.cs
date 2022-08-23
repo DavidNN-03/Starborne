b0cfe880fd87f11d2fbf0ee838a4f2c9407fd680
David Nielsen <ninjadaviddk@hotmail.com>
2022-08-23 15:11:19 +0200

    Bug fixed - onDeath can now only be called once before the object is destroyed

--- Assets/Scripts/GameResources/EnemyHealth.cs
+++ Assets/Scripts/GameResources/EnemyHealth.cs
@@ -8,14 +8,16 @@ namespace Starborne.GameResources
     public class EnemyHealth : MonoBehaviour
     {
         [SerializeField] private float health;
+        bool isDead = false;
 
         public event Action onDeath;
 
         public void TakeDamage(float damage)
         {
             health -= damage;
-            if (health <= 0)
+            if (!isDead && health <= 0) //the isDead check should remove the bug that onDeath can be called multiple times before the object is destroyed
             {
+                isDead = true;
                 onDeath();
             }
         }
